0bcb64b97f97255dff8ec7f3aa4e4906446954ce
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2024-05-29 15:16:12 +0200

    Add constants and platform cracking

--- base.h
+++ base.h
@@ -14,4 +14,42 @@ typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
 
+typedef float f32;
+typedef double f64;
+
+// NOTE: Constants
+const s8 min_s8 = (s8)0x80;
+const s16 min_s16 = (s16)0x8000;
+const s32 min_s32 = (s32)0x80000000;
+const s64 min_s64 = (s64)0x8000000000000000LL;
+
+const s8 max_s8 = (s8)0x7f;
+const s16 max_s16 = (s16)0x7fff;
+const s32 max_s32 = (s32)0x7fffffff;
+const s64 max_s64 = (s64)0x7fffffffffffffffLL;
+
+const u8 max_u8 = (u8)0xff;
+const u16 max_u16 = (u16)0xffff;
+const u32 max_u32 = (u32)0xffffffff;
+const u64 max_u64 = (u64)0xffffffffffffffffULL;
+
+// NOTE: Platform cracking
+#if defined(__linux__)
+#define IS_LINUX 1
+#else
+#define IS_LINUX 0
+#endif
+
+#if defined(_WIN32)
+#define IS_WINDOWS 1
+#else
+#define IS_WINDOWS 0
+#endif
+
+#if defined(__APPLE__) && defined(__MACH__)
+#define IS_MACOS 1
+#else
+#define IS_MACOS 0
+#endif
+
 #endif // BASE_H
